3a50d31de735f950cf871c610a898e203d42056d
rose <aom_bnn@hotmail.com>
2023-08-28 20:42:34 -0400

    Initial commit

---
+++ src/test/java/com/example/toiletApp/ToiletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toiletApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
